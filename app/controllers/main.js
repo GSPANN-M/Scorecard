3e5048020c1f5c27d597a250f0493a475cf3cb87
Manoj Kumar <mano.mykingdom@gmail.com>
2014-12-30 01:14:47 +0530

    android fixes

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,1 +1,38 @@
 var args = arguments[0] || {};
+
+function didClickTile(e) {
+	var action = e.source.action;
+	console.log("choosed : ", action);
+	switch(action) {
+	case "industry_leadership":
+		break;
+	case "innovative_products":
+		break;
+	case "timely_solutions":
+		break;
+	case "partnering_for_mutual_benefit":
+		break;
+	case "tool_upgrades":
+		break;
+	case "equipment_performance":
+		break;
+	case "tool_quality":
+		break;
+	case "spares_performance":
+		break;
+	case "service_contracts":
+		break;
+	case "technical_support":
+		break;
+	case "meeting_commitments":
+		break;
+	case "ease_of_doing_business":
+		break;
+	case "fast_response_to_escalations":
+		break;
+	case "tool_installation":
+		break;
+	case "on_time_delivery":
+		break;
+	}
+}
