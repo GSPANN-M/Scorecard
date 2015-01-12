d28771efd0139385e6c182631feaa253d4917af6
Manoj Kumar <mano.mykingdom@gmail.com>
2015-01-12 10:15:00 +0530

    coach marks landscape

--- app/controllers/coachMarksWin.js
+++ app/controllers/coachMarksWin.js
@@ -1,4 +1,20 @@
-var args = arguments[0] || {};
+var args = arguments[0] || {},
+    app = require("core");
+
+didOrientationChangeCoachMark();
+
+function didOpen(e) {
+	Ti.App.addEventListener("orientationChange", didOrientationChangeCoachMark);
+}
+
+function didClose(e) {
+	Ti.App.removeEventListener("orientationChange", didOrientationChangeCoachMark);
+}
+
+function didOrientationChangeCoachMark(e) {
+	$.img.image = "/images/coach_marks_".concat(app.device.orientation).concat(".png");
+	console.log($.img.image);
+}
 
 function didClick(e) {
 	$.coachMarksWin.close();
